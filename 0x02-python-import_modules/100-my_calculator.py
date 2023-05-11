8091a0afa943589aba76f08ffcb8774fbcb2f83f
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-05-11 10:47:06 -0400

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
 """Handle basic arithmetic operations."""
 
 from calculator_1 import add, sub, mul, div
 import sys
 
-if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
