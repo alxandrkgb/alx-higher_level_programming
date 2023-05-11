c4b7707de69a6053ca56acd1d388cf8fbecc22c7
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-05-11 10:44:20 -0400

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
+"""Handle basic arithmetic operations."""
 
-    if len(sys.argv) - 1 != 3:
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    ops = {"+", "-", "*", "/"}
+    if sys.argv[2] not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
