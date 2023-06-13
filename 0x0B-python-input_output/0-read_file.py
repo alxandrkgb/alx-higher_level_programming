b1b5bc677678cf5932ffb28bd84bb3b221ce2be8
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-06-13 08:50:33 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""
-Contains the read_file function
-"""
-
 
 def read_file(filename=""):
-    """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, 'r') as file:
+        contents = file.read()
+        print(contents, end='')
