3f8463d5e863c946bcdb033d9117e974b62d2dc4
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-07-19 17:05:04 -0700

    Initial commit

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- 9-cities_by_state_join.sql
+USE hbtn_0d_usa;
+
+-- List all cities with their respective states
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
