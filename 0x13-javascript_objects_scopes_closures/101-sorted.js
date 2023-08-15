723e55d9355c20020b8f803409485c1071d66d5f
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-08-15 14:28:45 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+const data = require('./101-data');
+
+const occurrencesById = data.dict;
+const idsByOccurrence = {};
+
+for (const userId in occurrencesById) {
+  const occurrence = occurrencesById[userId];
+  if (idsByOccurrence[occurrence] === undefined) {
+    idsByOccurrence[occurrence] = [];
+  }
+  idsByOccurrence[occurrence].push(userId);
+}
+
+console.log(idsByOccurrence);
