7cff6be4c63277c2cc9c44e1c45e0ce1a885fbcc
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-08-15 14:32:57 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
+
+const content1 = fs.readFileSync(sourceFile1, 'utf-8');
+const content2 = fs.readFileSync(sourceFile2, 'utf-8');
+const concatenatedContent = content1 + content2;
+
+fs.writeFileSync(destinationFile, concatenatedContent);
