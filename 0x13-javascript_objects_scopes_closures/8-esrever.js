8d2f0bd71af0b6d0a8bf8c1242d0002c54a5aad0
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-08-16 04:20:37 -0400

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,11 @@
+#!/usr/bin/node
+
 exports.esrever = function (list) {
-  const reversedList = [];
-  
-  for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
+  const revList = [];
+
+  for (let i = list.length - 1; i >= 0; --i) {
+    revList.push(list[i]);
   }
-  
-  return reversedList;
+
+  return revList;
 };
