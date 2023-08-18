1a49023e590f4d284d392d66ac7676a329b87f7b
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-08-18 09:49:59 -0400

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,41 +1,18 @@
-#!/usr/bin/env python3
-import sys
-import MySQLdb
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-    
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        
-        # Create a cursor to execute SQL queries
-        cursor = db.cursor()
-        
-        # Execute the query to fetch states starting with 'N'
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        
-        # Fetch all rows and display them
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-        
-        # Close the cursor and the database connection
-        cursor.close()
-        db.close()
-        
-    except MySQLdb.Error as e:
-        print("Error: {}".format(e))
-        sys.exit(1)
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
