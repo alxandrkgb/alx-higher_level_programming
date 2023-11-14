cb89c0c34c0979accdae63816de5aa66820f220c
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-11-14 13:57:40 -0800

    task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  return function (number) {
+    // Use toString method to convert the number to the specified base
+    return number.toString(base);
+  };
+};
